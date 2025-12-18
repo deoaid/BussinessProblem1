58c39dee6b3908f3d87dc51be563857cb2b719f7
deoaid <aidend46@nycstudents.net>
2025-12-18 11:54:45 -0500

    Update BussinessProblem.py

--- BussinessProblem.py
+++ BussinessProblem.py
@@ -7,19 +7,20 @@ root = Tk()
 root.title("Enter Title Here")
 
 #Set size of window
-root.geometry("425x550")
+root.geometry("400x500")
 
 # Create buttons
 title = Button(root, text="NoteTaker", background='white')
 file = Button(root, text="File", background='white')
 save = Button(root, text="Save", background='white')
 newtab = Button(root, text="New Tab", background='white')
-
+options = Button(root, text="color  font", background='white')
 
 
 # Place widgets in window (with pack function!)
 title.grid(row=0, column=0)
 file.grid(row=0, column=1)
+options.grid(row=0, column=1)
 save.grid(row=0, column=2)
 newtab.grid(row=0, column=3)
 
