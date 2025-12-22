c92b9988a568e4755441be3b8eb0eb98ac0751c6
deoaid <aidend46@nycstudents.net>
2025-12-22 14:21:21 -0500

    d
    d

--- BussinessProblem.py
+++ BussinessProblem.py
@@ -14,7 +14,8 @@ title = Button(root, text="NoteTaker", background='white')
 file = Button(root, text="File", background='white')
 save = Button(root, text="Save", background='white')
 newtab = Button(root, text="New Tab", background='white')
-options = Button(root, text="Color  Font", background='white')
+options = Button(root, text="Color Font", background='white')
+text = Text(root)
 
 
 # Place widgets in window (with pack function!)
@@ -23,6 +24,7 @@ file.grid(row=0, column=1)
 options.grid(row=1, column=1)
 save.grid(row=0, column=2)
 newtab.grid(row=0, column=3)
+text.grid(row=1, column=0)
 
 
 
